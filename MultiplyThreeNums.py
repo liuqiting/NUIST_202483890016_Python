8cc6415aa0b282fa766c60b36f7b8defb7f7d5b4
liuqiting <3481258900@qq.com>
2025-03-22 00:41:27 +0800

    Add files via upload

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,11 @@
+def multiply_three_nums(num1, num2, num3):
+    return num1 * num2 * num3
+
+
+if __name__ == "__main__":
+    num1 = 2
+    num2 = 3
+    num3 = 4
+    result = multiply_three_nums(num1, num2, num3)
+    print(f"three numbers {num1}、{num2} and {num3} multiplication: {result}")
+    
